659898f03f62f1ed868a1c879e0f1431ee960ebc
Gustavo <gustavo.4lv5s@gmail.com>
2020-09-03 17:10:05 -0300

    chore: pages

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <h2>Posts will show up here, later on.</h2>
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact page</h1>
+      <p>In progress.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
